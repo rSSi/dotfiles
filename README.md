68df2b4b3cfcf5d54127dcf1bfb226ba7480a811
rSSi <rossi@klingt.org>
2021-01-31 20:23:14 +0100

    initial commit - not quite...

---
+++ README.md
@@ -0,0 +1,4 @@
+#dotfiles
+
+This is my version of the dotfiles I use on my machines.
+Probably nothing new or special :)
--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ ln -s ~/.dot/.oh-my-zsh/ ~/.oh-my-zsh
 ln -s ~/.dot/.tmux.conf ~/.tmux.conf
 ln -s ~/.dot/.vim/.vimrc ~/.vimrc
 ln -s ~/.dot/.zshrc ~/.zshrc
+
+echo "Pulling Vim Plugins"
+git clone https://github.com/scrooloose/nerdtree.git ~/.dot/.vim/bundle/nerdtree
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.dot/.vim/bundle/Vundle.vim
