c66fc2b20ede517e7103111751a8a9c44c9362d1
rSSi <rossi@klingt.org>
2021-05-08 09:19:39 +0200

    added tpm install

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ ln -s ~/.dot/.tmux.conf ~/.tmux.conf
 ln -s ~/.dot/.vim/.vimrc ~/.vimrc
 ln -s ~/.dot/.zshrc ~/.zshrc
 
+echo "Pulling TMUX Plugin Manager"
+git clone https://github.com/tmux-plugins/tpm ~/.dot/.tmux/plugins/tpm
+
 echo "Pulling Vim Plugins"
 git clone https://github.com/scrooloose/nerdtree.git ~/.dot/.vim/bundle/nerdtree
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.dot/.vim/bundle/Vundle.vim
